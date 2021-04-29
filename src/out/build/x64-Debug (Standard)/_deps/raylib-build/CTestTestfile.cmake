13d1b8d7ff617571d83bdb79b1469668f1517ced
Cris Kohn <cristian.kohn@hotmail.com>
2021-04-29 09:47:04 +0200

    out folder deleted

--- src/out/build/x64-Debug (Standard)/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/crist/source/repos/MoodyCat1/MoodyCat1/src/out/build/x64-Debug (Standard)/_deps/raylib-src
-# Build directory: C:/Users/crist/source/repos/MoodyCat1/MoodyCat1/src/out/build/x64-Debug (Standard)/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
